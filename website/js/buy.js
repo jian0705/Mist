b136c8f9b57f05e68a1c1ae55f06c9e1c3577b9f
jian0705 <jimmy.andersson@vbu.se>
2025-04-10 11:31:11 +0200

    Add files via upload

---
+++ website/js/buy.js
@@ -0,0 +1,16 @@
+const param = new URLSearchParams(window.location.search)
+const item = param.get("item");
+
+const images = {
+    repo: "photo/repoLogo.avif",
+    lethalCompany: "photo/lethalCompanyLogo.webp",
+    minecraft2: "photo/minecraftTwoLogo.jpg",
+    securityBreach: "photo/SBLogo.webp",
+    terraria: "photo/terrariaLogo.jpg",
+    payday3: "photo/paydayThreeLogo.jpg",
+    fortnite: "photo/fortniteLogo.jfif",
+    darkSouls: "photo/darkSoulsLogo.jpg",
+    helloNeighbor: "photo/helloNeighborLogo.jpg",
+    amongUs: "photo/amongUsLogo.jpg",
+  };
+const imgURL = images[item]
