878d0d91ece9486a621abca20396988e7526a400
jian0705 <jimmy.andersson@vbu.se>
2025-05-14 14:30:27 +0200

    Add files via upload

--- website/js/cart.js
+++ website/js/cart.js
@@ -1,5 +1,25 @@
 let cart = sessionStorage.getItem('content')
 let basePrice = sessionStorage.getItem('price')
 
-//Number(basePrice)
-//JSON.parse(cart)
+let parsedBasePrice = Number(basePrice)
+let cartContent = JSON.parse(cart)
+
+let itemList = document.getElementById("itemList")
+let itemListContent = {}
+
+
+for (let i = 0; i<cartContent.length; i++){
+
+    if (cartContent[i] in itemListContent){
+        itemListContent[cartContent[i]] += 1
+        document.getElementById(cartContent[i]).textContent = cartContent[i] + ": " + itemListContent[cartContent[i]]
+    }else{
+        itemListContent[cartContent[i]] = 1
+        let li = document.createElement('li')
+
+        li.textContent = cartContent[i]
+        li.id = String(cartContent[i])
+
+        itemList.appendChild(li)
+    }
+}
